eb5e161625abadda4fb94839ed1aa50496925143
ujjal zaman <muzzwal97@gmail.com>
2021-11-18 11:30:45 +0000

    added services

--- src/component/Dashoboard/MakeAdmin/MakeAdmin.jsx
+++ src/component/Dashoboard/MakeAdmin/MakeAdmin.jsx
@@ -2,32 +2,31 @@ import axios from 'axios';
 import React, { useContext } from 'react';
 import { Col, Form, Row } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-// import toast from 'react-hot-toast';
-// import swal from 'sweetalert';
-// import { UserContext } from '../../../../App';
+import toast from 'react-hot-toast';
+import swal from 'sweetalert';
+import { UserContext } from '../../../App';
 
 const MakeAdmin = () => {
-    // const { user: {email} } = useContext(UserContext)
+    const { user: {email} } = useContext(UserContext)
     const { register, handleSubmit, formState: { errors }, reset} = useForm();
 
     const onSubmit = data => {
-        // const loading = toast.loading('Please wait...')
-        
-        // if(email === "test@admin.com"){
-        //     toast.dismiss(loading)
-        //     swal("Permission restriction!", "As a test admin, You haven't permission to add a new admin", "info");
-        // } else {
-        //     axios.post('https://trusted-tech.herokuapp.com/addAdmin',{email: data.email})
-        //     .then(res => {
-        //         toast.dismiss(loading)
-        //         toast.success('One admin added successfully')
-        //         reset();
-        //     })
-        //     .catch(err => {
-        //         toast.dismiss(loading)
-        //         toast.error(err.message)
-        //     })
-        // }
+        const loading = toast.loading('Please wait...')
+        if(email === "test@admin.com"){
+            toast.dismiss(loading)
+            swal("Permission restriction!", "As a test admin, You haven't permission to add a new admin", "info");
+        } else {
+            axios.post('https://immense-river-40491.herokuapp.com/addAdmin',{email: data.email})
+            .then(res => {
+                toast.dismiss(loading)
+                toast.success('One admin added successfully')
+                reset();
+            })
+            .catch(err => {
+                toast.dismiss(loading)
+                toast.error(err.message)
+            })
+        }
     };
     return (
         <div className="px-2">
--- src/component/Dashoboard/ManageServices/ManageServices.jsx
+++ src/component/Dashoboard/ManageServices/ManageServices.jsx
@@ -1,24 +1,24 @@
-// import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useContext } from 'react';
 import { Table, Button } from 'react-bootstrap';
-// import toast from 'react-hot-toast';
-// import swal from 'sweetalert';
-// import { UserContext } from '../../../../App';
+import toast from 'react-hot-toast';
+import swal from 'sweetalert';
+import { UserContext } from '../../../App';
 import TableLoader from '../../Shared/TableOrder/TableOrder';
 import AddService from '../AddService/AddService';
 
 const ManageServices = () => {
-    // const {user : { email }} = useContext(UserContext)
+    const {user : { email }} = useContext(UserContext)
     const [services, setServices] = useState([])
     const [isUpdated, setIsUpdated] = useState(false)
     const [edit, setEdit] = useState(null);
 
     useEffect(() => {
-        axios.get('https://trusted-tech.herokuapp.com/services')
+        axios.get('https://immense-river-40491.herokuapp.com/services')
         .then(res => {
             setServices(res.data);
             setIsUpdated(false)
@@ -26,54 +26,53 @@ const ManageServices = () => {
     }, [isUpdated, edit])
     
     const checkPermission = (id, action) => {
-
-        // const getMainServices = services.slice(0, 6)
-        // const getService = getMainServices.find(({_id}) => id === _id)
+        const getMainServices = services.slice(0, 6)
+        const getService = getMainServices.find(({_id}) => id === _id)
         
-        // if(getService && email === "test@admin.com"){
-        //     swal("Permission restriction!","As a test admin, you can't edit or delete the main six services. You can only edit or delete your added services", "info" )
-        // } else {
-        //     if(action === 'edit'){
-        //         setEdit(id)
-        //     } else {
-        //         handleDelete(id)
-        //     }
-        // }
+        if(getService && email === "test@admin.com"){
+            swal("Permission restriction!","As a test admin, you can't edit or delete the main six services. You can only edit or delete your added services", "info" )
+        } else {
+            if(action === 'edit'){
+                setEdit(id)
+            } else {
+                handleDelete(id)
+            }
+        }
     }
 
     const handleDelete = (id) => {
-        // setIsUpdated(false)
-        // swal({
-        //     title: "Are you sure?",
-        //     text: "Are you sure! you want to delete this service?",
-        //     icon: "warning",
-        //     buttons: true,
-        //     dangerMode: true,
-        //   })
-        //   .then( wantDelete => {
-        //     if (wantDelete) {
-        //         const loading = toast.loading('deleting...Please wait!')
-        //         axios.delete(`https://trusted-tech.herokuapp.com/delete/${id}`)
-        //         .then(res => {
-        //             toast.dismiss(loading)
-        //             if(res){
-        //                 setIsUpdated(true);
-        //                 toast.success('Service has been deleted successfully!');
-        //             }
-        //             else{
-        //                 toast.error('Something went wrong, please try again');
-        //             }
-        //         })
-        //         .catch(err => {
-        //             toast.dismiss(loading)
-        //             swal({
-        //                 title: "Failed!",
-        //                 text: 'Something went wrong, please try again',
-        //                 icon: "error",
-        //               });
-        //         })
-        //     } 
-        //   })
+        setIsUpdated(false)
+        swal({
+            title: "Are you sure?",
+            text: "Are you sure! you want to delete this service?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then( wantDelete => {
+            if (wantDelete) {
+                const loading = toast.loading('deleting...Please wait!')
+                axios.delete(`https://immense-river-40491.herokuapp.com/delete/${id}`)
+                .then(res => {
+                    toast.dismiss(loading)
+                    if(res){
+                        setIsUpdated(true);
+                        toast.success('Service has been deleted successfully!');
+                    }
+                    else{
+                        toast.error('Something went wrong, please try again');
+                    }
+                })
+                .catch(err => {
+                    toast.dismiss(loading)
+                    swal({
+                        title: "Failed!",
+                        text: 'Something went wrong, please try again',
+                        icon: "error",
+                      });
+                })
+            } 
+          })
     }
 
     return (
@@ -89,8 +88,8 @@ const ManageServices = () => {
                     <thead>
                         <tr>
                             <th>Name</th>
-                            <th>Price</th>
                             <th>Description</th>
+                            <th>Price</th>
                             <th>Action</th>
                         </tr>
                     </thead>
@@ -98,18 +97,18 @@ const ManageServices = () => {
                         {
                             services.map(({_id, name, price, description}) => {
                                 let des = description
-                                let shortDes = des.split(' ').slice(0,5).join(' ')
+                                // let shortDes = des.split(' ').slice(0,5).join(' ')
                                 return(
                                     <tr>
                                         <td>{name}</td>
-                                        <td>{`${shortDes}...`}</td>
+                                        <td>{des ? des : "Nothing"}</td>
                                         <td>${price}</td>
                                         <td>
                                             <Button variant="outline-success" onClick={() => checkPermission(_id, 'edit')}> 
-                                            {/* <FontAwesomeIcon icon={faEdit}/> */}
+                                            <FontAwesomeIcon icon={faEdit}/>
                                              Edit</Button>
                                             <Button className="ml-2" variant="outline-danger" onClick={() => checkPermission(_id, 'delete')}> 
-                                            {/* <FontAwesomeIcon icon={faTrashAlt}/> */}
+                                            <FontAwesomeIcon icon={faTrashAlt}/>
                                              Delete</Button>
                                         </td>
                                     </tr>
